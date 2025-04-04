49210fb23e0a50af245cbf93b7b57a5b5a462f4b
NoNeedToCode <1946530045@qq.com>
2025-04-05 00:13:36 +0800

    Add files via upload

---
+++ 10086/2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read your progrom later.
+#Anything after # is ignored by python.
+
+print("I could have code this.")# and after the comment is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print(This can't run.)
+
+print("this will run.")
